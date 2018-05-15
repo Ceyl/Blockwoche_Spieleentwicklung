263c834c96430f5b454737d3a94084e1c5ddf9fc
Holger Schad <holgerschad@outlook.de>
2018-05-15 10:54:03 +0200

    added winning screen and modified time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     [SerializeField] private GameObject timeText;
     [SerializeField] private GameObject winnerText;
+    [SerializeField] private GameObject winnerNameText;
+    [SerializeField] private GameObject restartButton;
 
     [SerializeField] private float timeLeft = 300f;
+    private bool timeColorChanged;
     private bool gameOver;
     private int minutes;
     private int seconds;
@@ -16,6 +20,7 @@ public class GameManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         gameOver = false;
+        timeColorChanged = false;
     }
 	
 	// Update is called once per frame
@@ -35,7 +40,11 @@ public class GameManager : MonoBehaviour {
         string secondsInString = seconds < 10 ? "0" + seconds : "" + seconds;
 
         timeText.GetComponent<Text>().text = string.Format("{0}:{1}", minutes, secondsInString);
-
+        if (minutes == 0 && seconds < 10 && !timeColorChanged)
+        {
+            timeText.GetComponent<Text>().color = Color.red;
+            timeColorChanged = true;
+        }
         if (minutes == 0 && seconds == 0)
         {
             gameOver = true;
@@ -51,5 +60,14 @@ public class GameManager : MonoBehaviour {
         {
             winner = players[i].transform.position.y > winner.transform.position.y ? players[i] : winner;
         }
+        winnerText.SetActive(true);
+        winnerNameText.GetComponent<Text>().text = winner.name;
+        winnerNameText.SetActive(true);
+        restartButton.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
