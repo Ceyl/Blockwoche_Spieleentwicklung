f112946478997a3c7247930719f204e53b6c67a0
Holger Schad <holgerschad@outlook.de>
2018-05-15 13:01:02 +0200

    added camerabehavior and playerspawn

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    private Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+	}
+	
+    void Update ()
+    {
+        MoveCamera();
+        OutOfCamera();
+    }
+
+    private void MoveCamera()
+    {
+        if (transform.position.y > mainCamera.transform.position.y + (mainCamera.transform.localScale.y * mainCamera.orthographicSize))
+        {
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y - (mainCamera.transform.localScale.y * mainCamera.orthographicSize), mainCamera.transform.position.z);
+        }
+    }
+
+    private void OutOfCamera()
+    {
+        if((transform.position.y + transform.localScale.y / 2) < mainCamera.transform.position.y - (mainCamera.transform.localScale.y * mainCamera.orthographicSize))
+        {
+            FindObjectOfType<GameManager>().CheckForExistingPlayers(gameObject);
+        }
+    }
+
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,17 +10,33 @@ public class GameManager : MonoBehaviour {
     [SerializeField] private GameObject winnerText;
     [SerializeField] private GameObject winnerNameText;
     [SerializeField] private GameObject restartButton;
+    [SerializeField] private GameObject playerPrefab;
 
     [SerializeField] private float timeLeft = 300f;
+    [Range(2, 4)] [SerializeField] private int playerCount = 2;
     private bool timeColorChanged;
     private bool gameOver;
     private int minutes;
     private int seconds;
+    private Camera mainCamera;
+    private Color[] colors = new Color[] {Color.blue, Color.red, Color.green, Color.yellow };
 
     // Use this for initialization
     void Start () {
         gameOver = false;
         timeColorChanged = false;
+        mainCamera = Camera.main;
+        // calculate distance to spawn chars in same distance to each other
+        float distance = (2 * (mainCamera.orthographicSize - mainCamera.orthographicSize * 0.1f)) / playerCount;
+        float tmpDistance = -(mainCamera.orthographicSize - mainCamera.orthographicSize * 0.1f) + distance / 2;
+        // spawn players on map
+        for (int i = 0; i < playerCount; i++)
+        {
+            GameObject player = Instantiate(playerPrefab, new Vector3(tmpDistance, 2, 0), Quaternion.identity);
+            player.name = string.Format("Player{0}", i);
+            player.GetComponent<SpriteRenderer>().color = colors[i];
+            tmpDistance += distance;
+        }
     }
 	
 	// Update is called once per frame
@@ -28,34 +44,42 @@ public class GameManager : MonoBehaviour {
 
         if(!gameOver)
             CheckTime();
+        else
+            GameOver();
 
     }
 
     private void CheckTime()
     {
+        // calculate time for time text
         timeLeft -= Time.deltaTime;
         minutes = (int)timeLeft / 60;
         seconds = (int)timeLeft % 60;
 
         string secondsInString = seconds < 10 ? "0" + seconds : "" + seconds;
-
         timeText.GetComponent<Text>().text = string.Format("{0}:{1}", minutes, secondsInString);
+
+        // check if time is nearly over to color time text
         if (minutes == 0 && seconds < 10 && !timeColorChanged)
         {
             timeText.GetComponent<Text>().color = Color.red;
             timeColorChanged = true;
         }
+
+        // check if time is over to calculate winner
         if (minutes == 0 && seconds == 0)
         {
             gameOver = true;
-            GameOver();
         }
     }
 
     private void GameOver()
     {
+        // get all player objects which are still in scene
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         GameObject winner = players[0];
+
+        // check players y position to calculate winner
         for (int i = 1; i < players.Length; i++)
         {
             winner = players[i].transform.position.y > winner.transform.position.y ? players[i] : winner;
@@ -68,6 +92,15 @@ public class GameManager : MonoBehaviour {
 
     public void Restart()
     {
+        // restart level
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CheckForExistingPlayers(GameObject player)
+    {
+        // destroys player if the character is completely outside of the camera
+        DestroyImmediate(player);
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        gameOver = players.Length == 1 ? true : false;
     }
 }
